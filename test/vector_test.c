b12650c85c0993d6cc21168089e2122d4b7d6ddd
0b01010100 <74110017+0b01010100@users.noreply.github.com>
2024-09-05 04:06:50 -0400

    adding vvector

--- test/vector_test.c
+++ test/vector_test.c
@@ -1,21 +1,12 @@
-// #include <stdio.h>
-// #include <vblock.h>
-// #include <vstr.h>
-// #include <vstring.h>
-// #include <vfs.h>
-// #include <vstack.h>
-// #include <vdef.h>
-// #include <varray.h>
-// #include <vvector.h>
 
-// int main() 
-// {   
-//     vvector1* d = vvector1_create(int, 2, 0.75, NULL, NULL, NULL);
-//     int a = 29;
-//     vvector_push_front(d, &a);
-//     vvector_push_front(d, &a);
-//     vvector_emplace_back(d, &a);
-//     const int* t = vvector_at(d, 2);
-//     printf("%i", *t);
-//     return 0;
-// }
+#include <vvector.h>
+
+int main() 
+{   
+    vvector1* d = vvector1_create(int, 2, 0.75, NULL, NULL, NULL);
+    int a = 29;
+    vvector_push_back(d, &a);
+    const int* t = vvector_at(d, 0);
+    printf("%i", *t);
+    return 0;
+}
